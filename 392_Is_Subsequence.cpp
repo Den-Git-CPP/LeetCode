0f0dc42597c2e9316f0c5048834a4beb94e7d33c
Den-Git-CPP <madim-neodart@rambler.ru>
2023-01-17 14:48:45 +0300

    Add 392_Is_Subsequence.cpp CmakeLists.txt

--- 392_Is_Subsequence.cpp
+++ 392_Is_Subsequence.cpp
@@ -1,20 +1,28 @@
 #include <iostream>
 #include <string>
-#include <vector>
-
-using namespace std;
-class Solutions{
+class Solution {
 public:
-  Solutions(/* args */) {}
-  ~Solutions() {}
-   
+   bool isSubsequence( const std::string &s, const std::string &t) {
+		if (s.empty()){	return true;}
+    	if (t.empty()) { return false; }
+        int i{0};
+        for(char c:t){
+            if(c==s[i]){
+                i++;
+                if(i==(int)s.size()){
+                    return true;
+                }
+            }
+        }
+	return false;
+	}
 };
 
-
 int main(int argc, char* argv[])
-{   
-    Solutions s1; 
+{
+	Solution s1;
+	std::cout << std::boolalpha << s1.isSubsequence("abc", "ahbgdc") << "\n";
+	std::cout << std::boolalpha << s1.isSubsequence("axc", "ahbgdc") << "\n";
 
-  
-return 0;
+	return 0;
 }
